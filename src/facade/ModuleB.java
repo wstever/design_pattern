55708b0587df69cd012c77fb43f13d56adacd0fa
wstever <415251475@qq.com>
2017-04-10 10:58:56 +0800

    原来门面模式其实就是我们经常有用到的,一个入口~(拍照只要按下快门,其他都不管~)

---
+++ src/facade/ModuleB.java
@@ -0,0 +1,8 @@
+package facade;
+
+public class ModuleB {
+	// 示意方法
+		public void testB() {
+			System.out.println("调用ModuleB中的testB方法");
+		}
+}
---
+++ src/facade/ModuleC.java
@@ -0,0 +1,8 @@
+package facade;
+
+public class ModuleC {
+	// 示意方法
+		public void testC() {
+			System.out.println("调用ModuleC中的testC方法");
+		}
+}
